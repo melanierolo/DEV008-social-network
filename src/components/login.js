8eea3c5d85f08eca92549fce363ab9bc5fb39087
Melanie Rodas <mrodasl@uni.pe>
2023-07-04 18:26:31 -0500

    added css on the Login page

--- src/components/login.js
+++ src/components/login.js
@@ -18,32 +18,34 @@ export const Login = (onNavigate) => {
   logindiv.appendChild(buttonLogin);*/
 
   //Template Strings
-  const loginLogo = `<div class="login">
+  const loginLogo = `<div class="loginLogo">
                         <img class="loginLogo__img" src="./assets/images/catsSociety--logo.png" alt="logo CatsSociety" />
                         <h2 class="loginLogo__title">CatsSociety</h2>
                       </div>`;
 
   const loginForm = `<form class="loginForm" id="loginForm">
-                        <label class="loginForm__label" for="userEmail">Email:</label>
-                        <input class="loginForm__input" type="email" id="userEmail" name="userEmail" placeholder="John@email.com">
-                        <label class="loginForm__label" for="userPassword">Contraseña:</label>
-                        <input class="loginForm__input" type="password" id="userPassword" name="userPassword" placeholder="Contraseña">
-                        <a class="loginForm__link loginForm__link--black">¿Olvidaste la contreseña?</a>  
-                        <input id="btnLogin" class="btn btn--primary" type="submit" value="Iniciar Sesión">
+                        <label class="loginForm__label marginBottom_4" for="userEmail">Email:</label>
+                        <input class="loginForm__input marginBottom_4" type="email" id="userEmail" name="userEmail" placeholder="John@email.com">
+                        <label class="loginForm__label marginBottom_4" for="userPassword">Contraseña:</label>
+                        <input class="loginForm__input marginBottom_4" type="password" id="userPassword" name="userPassword" placeholder="Contraseña">
+                        <a class="loginForm__link loginForm__link--black marginBottom_16">¿Olvidaste la contraseña?</a>  
+                        <input id="btnLogin" class="btn btn--primary marginBottom_8" type="submit" value="Iniciar Sesión">
                       </form>`;
 
   const loginButtons = `<div class="loginButtons">
-                          <h4 class="loginButtons__o">o</h4>
-                          <button class="btn btn--google">Continua con Google</button>
+                          <h4 class="loginButtons__o marginBottom_8">o</h4>
+                          <button class="btn btn--google marginBottom_16">Continua con Google</button>
                           <p>¿No tienes una cuenta? <a class="loginForm__link loginForm__link--blue" id="linkRegister">Regístrate</a></p>
                         </div>`;
 
-  const login = `<section class=”sectionLeft”>
+  const login = `<section class="sectionLeft">
                   </section>
-                  <section class=”sectionRight>
+                  <section class="sectionRight">
+                   <div class="login">
                     ${loginLogo}
                     ${loginForm}
                     ${loginButtons}
+                   </div>
                   </section>`;
 
   const loginDiv = document.createElement("div");
