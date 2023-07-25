3a8f40e4f0a6f15ff9b19318a3e5d1c850adf26b
claucegoma <arzagitana@ciencias.unam.mx>
2023-07-24 19:03:11 -0600

    add style on the modal post edit

--- src/components/modalPostEdit.js
+++ src/components/modalPostEdit.js
@@ -6,9 +6,20 @@ export const ModalPostEdit = () => {
   const modal = `
     <!-- Modal content -->
     <div class="modal-content">
-      <input id="modal-text">
-      <button id="btn-modal-cancel" class="close">Cancelar</button>
-      <button id="btn-modal-save" class="save">Guardar</button>
+      <div class="modal-header">
+        <h3>Editar publicación</h3>
+        <button id="btn-modal-cancel" class="close">X</button>
+      </div>
+      <div class="modal-body"> 
+        <textarea class="modal-body-input" id="modal-text"> </textarea>
+      </div>
+      <div class="modal-buttons">
+        <div class="modal-buttons-icon"> 
+          <img class="modal-img" src="./assets/icons/img.png" alt="add photo">
+          <p>Agregar imagen</p>
+        </div> 
+        <button id="btn-modal-save" class="btn btn--primary btn-save">Guardar</button>
+      </div>
     </div>`;
   myModalPostDiv.innerHTML = modal;
   return myModalPostDiv;
