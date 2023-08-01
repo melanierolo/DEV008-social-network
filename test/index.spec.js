6187b3e75af8e51731550718335a5c23db813c8d
Melanie Rodas <mrodasl@uni.pe>
2023-08-01 07:45:49 -0500

    install jest and jest-environment-jest in the same version

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,1 +1,9 @@
 // importamos la funcion que vamos a testear
+/**
+ * @jest-environment jsdom
+ */
+
+test('use jsdom in this test file', () => {
+  const element = document.createElement('div');
+  expect(element).not.toBeNull();
+});
